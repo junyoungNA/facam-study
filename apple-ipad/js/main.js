c6ab64a7c14394b6b86877060a2a7627ac87c952
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-02 02:16:45 +0900

    검색바 스크롤 무시, 클릭시 검색바 생성, 취소

--- apple-ipad/js/main.js
+++ apple-ipad/js/main.js
@@ -18,15 +18,36 @@ basketEl.addEventListener('click', (event) => {
     event.stopPropagation();
 })
 
-window.addEventListener('click', () => {
-    //윈도우 클릭시 드롭다운 class remove show;
-    hideBasket();
-})
-
 const showBasket = () => {
     basketEl.classList.add('show');
 }
 
 const hideBasket = () => {
     basketEl.classList.remove('show');
+}
+
+window.addEventListener('click', hideBasket);
+    //윈도우 클릭시 드롭다운 class remove show;
+
+//검색
+const headerEl = document.querySelector('header');
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+    headerEl.classList.add('searching');
+    document.documentElement.classList.add('fixed');
+}
+
+function hideSearch() {
+    headerEl.classList.remove('searching');
+    document.documentElement.classList.add('fixed');
+
 }
