4278e39b86b95b65a0064869fd9a3c069374813c
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-03 01:43:08 +0900

    class 컴포넌트 porops 로 eventhandler 전달

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 import Square from './Square'
 import './Board.css';
 export default class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares : Array(9).fill(null), //state생성 초기값 null 지정
+        }
+    }
+    handleClick(i) {
+        const squares =this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares : squares})
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>
+        return <Square value={this.state.squares[i]} onClick= {() => this.handleClick(i)}/>
     }
     render() {
     return (
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './Square.css';
 
 export default class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value : null, //state생성 초기값 null 지정
-        }
-    }
+  
     render() {
         return (
-            <button className='square' onClick={() => this.setState({value :'x'})}>{this.state.value}</button>
+            <button className='square' onClick={() => this.props.onClick()}>{this.props.value}</button>
         ) 
         
     }
