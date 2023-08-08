5209ba0c3b9cacf722fc96e3e98f92724aceae59
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-08 22:31:46 +0900

    next-auth signin 함수 사용시 404에러

--- market/src/app/components/NavItem.tsx
+++ market/src/app/components/NavItem.tsx
@@ -1,13 +1,18 @@
+import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 const NavItem = ({mobile} : {mobile?: boolean} ) => {
+    const session = useSession();
     return (
         <ul className={`text-md justify-center flex gap-4 item-center ${mobile && "flex-col h-full" } `}>
             <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/a'>admin</Link></li>
             <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/a'>User</Link></li>
+            {session ?      
             <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signout</button></li>
-            <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signin</button></li>
+            :
+            <li className='py-2 text-center border-b-4 cursor-pointer'><button onClick={() => signIn() }>Signin</button></li>
+            }
         </ul>
     )
 }
--- market/src/app/layout.tsx
+++ market/src/app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
+'use client'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+import {SessionProvider} from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}</body>
+        <SessionProvider>
+          <Navbar/>
+          {children}
+        </SessionProvider>
+        </body>
     </html>
   )
 }
