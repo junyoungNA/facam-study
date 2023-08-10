40f834d5a25bc170a7e6099ec780df1529821bb7
junyoungNA <nanasooo@naver.com>
2023-08-10 19:51:52 +0900

    input 컴포넌트 스타일링

--- market/src/app/components/Input.tsx
+++ market/src/app/components/Input.tsx
@@ -1,6 +1,21 @@
+import { type } from 'os';
 import React from 'react'
+import { FieldValues, UseFormRegister,FieldErrors } from 'react-hook-form';
 
-const Input = ({
+interface InputProps {
+    id : string;
+    label : string;
+    type? : string;
+    disabled? : boolean;
+    formatPrice? : boolean;
+    required : boolean;
+    register : UseFormRegister<FieldValues>;
+    errors : FieldErrors
+
+
+}
+
+const Input: React.FC <InputProps> = ({
     id,
     label,
     type = 'text',
@@ -8,13 +23,55 @@ const Input = ({
     formatPrice,
     register,
     required,
-    erros,
+    errors,
 }) => {
     return (
-        <div>
-        
+        <div className='relative w-full'>
+            {formatPrice && 
+                <span className='absolute text-neutral-700 top-5'>₩</span>
+            }
+            <input 
+                id={id}
+                disabled={disabled}
+                {...register(id, {required})}
+                placeholder=''
+                type={type}
+                className={`
+                    w-full
+                    p-4
+                    pt-6
+                    font-light
+                    bg-white
+                    rounded-md
+                    outline-none
+                    transition
+                    disabled:opacity-70
+                    disabeld:cursor-not-allowed 
+                    ${formatPrice ? 'pl-9' : 'pl-4'}
+                    ${errors[id] ? 'border-rose-900' : 'border-neutral-300'}
+                    ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+                `}
+            />
+                <label className={`
+                    absolute
+                    text-md
+                    duration-150
+                    transform
+                    -translate-y-3
+                    top-5
+                    z-10
+                    origin-[0]
+                    ${formatPrice ? 'left-9': 'left:4'}
+                    peer-placeholder-shown:scale-100
+                    peer-placeholder-shown:translate-y-0
+                    peer-focus-:scale-75
+                    peer-focus:translate-y-4
+                    ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+                `}>
+                </label>
         </div>
     )
 }
 
 export default Input
+ 
