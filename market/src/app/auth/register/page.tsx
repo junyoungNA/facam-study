d46f66e4f15efd4ab3297ad6709904d6880d6eb3
junyoungNA <nanasooo@naver.com>
2023-08-10 20:31:38 +0900

    register 스타일 설정 중

--- market/src/app/auth/register/page.tsx
+++ market/src/app/auth/register/page.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-
+'use client'
+import React, {useState} from 'react';
+import {FieldValue, FieldValues, useForm} from 'react-hook-form';
+import Input from '@/app/components/Input';
 const RegisterPage = () => {
+    const [isLoading, setLoading] = useState(false);
+
+    const {register, handleSubmit, formState: {
+        errors,
+    }} = useForm<FieldValues>({
+        defaultValues : {
+            name : '',
+            email :'',
+            password:'',
+        }
+    })
+
     return (
-        <div>
-        
-        </div>
+        <section className='grid h-[calc(100vh_-_56px)] place-items-center border-black border-2'>
+            <form className='flex flex-col justify-center gap-4 min-w-[350px]'>
+                <h1 className='text-2xl'>Register</h1>
+                <Input 
+                    id='email'
+                    label='Email'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+                <Input 
+                    id='name'
+                    label='Name'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+                <Input 
+                    id='password'
+                    label='Password'
+                    type='password'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </form>
+        </section>
     )
 }
 
