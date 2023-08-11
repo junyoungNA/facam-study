4c375c784da6385bc0ea4928b16b0b1b24ffba0b
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-12 01:37:09 +0900

    market  좋아요 기능 완료 favorite route delete 기능 추가

--- market/src/app/api/favorites/[productId]/route.ts
+++ market/src/app/api/favorites/[productId]/route.ts
@@ -36,3 +36,39 @@ export async function POST(request:Request, {params} : {params: Params}) {
     })
     return NextResponse.json(user);
 }
+
+
+export async function DELETE(request:Request, {params} : {params: Params}) {
+    const currentUser = await getCurrentUser(); //유저 정보 가져오기
+
+    if(!currentUser) {
+        return NextResponse.error();
+    }
+    const {productId} = params; //url에서 params 가졍오기
+
+    //게시물 정보가 없거나 id가 stirng이 아니면 에러처리
+    if(!productId || typeof productId !== 'string') {
+        throw new Error('Invalid ID');
+    }
+    // 해당 유저가 좋아요를 눌렀다면 데이터베이스에 있는 데이터를, 
+    //없다면 빈 배열 
+    let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+    //filter를 통해 productId 를 찾아 걸러준다
+    favoriteIds = favoriteIds.filter((id) => id !== productId );
+
+    const user = await prisma?.user.update({
+        //유저 데이터베이스에서 유저의 id 컬럼을 찾아
+        //favoriteIds 컬럼 변경
+        where : {
+            id : currentUser.id
+        },
+        data : {
+            favoriteIds : favoriteIds
+        }
+    })
+
+
+
+    return NextResponse.json(user);
+}
