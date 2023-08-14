7266ba1a7aa3ff8f50f58456c5eae7195167cf55
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-14 17:03:22 +0900

    market 채팅 useSWRMutation 사용해서 캐싱 처리 및 더 ㅃ빠르게 변경사항 가져오기

--- market/src/app/components/chat/Input.tsx
+++ market/src/app/components/chat/Input.tsx
@@ -3,10 +3,25 @@ import React, { FormEvent } from 'react'
 import { useState } from 'react';
 import {IoImageOutline} from 'react-icons/io5'
 import {RiSendPlaneLine} from 'react-icons/ri'
+import useSWRMutation from 'swr/mutation';
 
 interface InputProps {
     receiverId : string;
     currentUserId : string;
+}
+
+const sendRequest = (url : string, {arg} : {
+    arg : {
+        text : string;
+        image : string;
+        senderId : string;
+        receiverId : string;
+    }
+}) => {
+    return fetch(url, {
+        method : 'POST',
+        body : JSON.stringify(arg)
+    }) .then(res => res.json())
 }
 
 const Input:React.FC<InputProps> = ({
@@ -15,6 +30,8 @@ const Input:React.FC<InputProps> = ({
 }) => {
 
     const [message, setMessage] = useState('');
+
+    const {trigger} = useSWRMutation('/api/chat', sendRequest)
 
     const handleSubmit = async (e : FormEvent<HTMLFormElement>)  => {
         e.preventDefault();
@@ -26,12 +43,12 @@ const Input:React.FC<InputProps> = ({
 
         if(message || imageUrl) {
             try {   
-                await axios.post('/api/chat', {
+                trigger({
                     text : message,
                     image : imageUrl,
                     receiverId : receiverId,
                     senderId : currentUserId,
-                })
+                    })
             }catch (error) {
                 console.error(error);
             }
